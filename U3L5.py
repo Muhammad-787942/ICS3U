92df852b7295d72684a56f8a626c4a61151ebf16
Muhammad-787942 <787942@pdsb.net>
2024-10-31 10:41:46 -0400

    Create U3L5.py

---
+++ U3L5.py
@@ -0,0 +1,16 @@
+#1
+import math 
+def multiply(a, b):
+  return a * b
+
+c = int(input("input a value here: "))
+d = int(input("input a value here: "))
+print (multiply(c, d))
+
+#2
+def myPow(m,n):
+  return math.pow (m, n)
+  
+x = int(input("input a value here: "))
+y = int(input("input a value here: "))
+print (myPow(x, y))
